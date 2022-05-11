e54140e253088ff25b9ffec746de2b58d36fa58a
mjjmorris <melaniemorrisjordan@protonmail.com>
2022-05-11 18:54:38 -0400

    passed all tests

---
+++ index.js
@@ -0,0 +1,51 @@
+const calculate = (input) => {
+  let calculations = [];
+
+  for (let index = 0; index < input.length; index++) {
+    const currentIndex = input[index];
+
+    if (isValid(currentIndex)) {
+      calculations.push(currentIndex);
+    } else if (currentIndex === null) {
+      calculations.push(0);
+    }
+  }
+  if (calculations.length !== 3) {
+    return NaN;
+  }
+
+  return perfCalculation(calculations);
+};
+const perfCalculation = (incomingCalcs) => {
+  switch (incomingCalcs[1]) {
+    case "+":
+      return Number(incomingCalcs[0]) + Number(incomingCalcs[2]);
+    case "-":
+      return Number(incomingCalcs[0]) - Number(incomingCalcs[2]);
+    case "*":
+      return Number(incomingCalcs[0]) * Number(incomingCalcs[2]);
+    case "/":
+      return Number(incomingCalcs[0]) / Number(incomingCalcs[2]);
+
+    default:
+      return NaN;
+  }
+};
+const isANumber = (number) => {
+  if (!isNaN(number) && number !== "") {
+    return true;
+  }
+
+  return false;
+};
+const isValid = (operator) => {
+  const validOperators = ["+", "-", "*", "/"];
+
+  if (validOperators.includes(operator) || isANumber(operator)) {
+    return true;
+  }
+
+  return false;
+};
+
+module.exports = calculate;
